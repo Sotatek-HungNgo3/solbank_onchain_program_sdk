68e008a23f4e61c1d0ce628a9a2ec46f714cae53
ThienNguyen <thien.nguyen@sotatek.com>
2021-12-06 23:48:02 +0700

    remove funding amount

--- src/utils/instructions.ts
+++ src/utils/instructions.ts
@@ -382,7 +382,6 @@ export class Instructions {
     },
     inputData: {
       outcoming_amount: number;
-      funding_amount: number;
     },
     poolProgramId: PublicKey,
   ): TransactionInstruction {
@@ -408,7 +407,6 @@ export class Instructions {
     const commandDataLayout = BufferLayout.struct([
       BufferLayout.u8('instruction'),
       Layout.uint64('outcoming_amount'),
-      Layout.uint64('funding_amount'),
     ]);
 
     let data = Buffer.alloc(1024);
@@ -417,7 +415,6 @@ export class Instructions {
         {
           instruction: 2, // Withdraw
           outcoming_amount: new Numberu64(inputData.outcoming_amount).toBuffer(),
-          funding_amount: new Numberu64(inputData.funding_amount).toBuffer(),
         },
         data,
       );
